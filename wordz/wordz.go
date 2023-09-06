b5c9886c405f19cf8c5e13d44fe4a870397575de
DevNoch <144225084+DevNoch@users.noreply.github.com>
2023-09-06 13:42:20 +0300

    Update wordz.go

--- wordz/wordz.go
+++ wordz/wordz.go
@@ -1,1 +1,21 @@
+package wordz
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+var Hello = "Hello it`s package wordz"
+var Prefix = "Random wordz"
+var Wordz = []string{"One", "Two", "Three", "Four"}
+
+func Random() string {
+	max := len(Wordz)
+	r, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	return get(r.Int64())
+}
+
+func get(index int64) string {
+	return Prefix + Wordz[index]
+}
 
